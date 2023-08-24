fc5a0e3eda0fe0ade8166f6def3b613fb164733c
bubun123-git <bubundcool@gmail.com>
2023-08-24 13:59:24 +0530

    async await

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-
+import React, { useState } from 'react';
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
+
+      const transformedMovies = data.results.map(movieData => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingtext: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }));
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
   return (
-    <div className="App">
-      Making a Movie project
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
